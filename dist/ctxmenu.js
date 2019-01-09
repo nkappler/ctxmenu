a268d17faec5448949a9f3c194c7816289ec2684
Nikolaj Kappler <nikolaj.kappler@sap.com>
2019-01-09 16:54:00 +0100

    first test demo where a menu is rendered
    context menu gets rendered in most rudimentary form (without css).
    context menu will disappear after two seconds since we don't have a
    management system yet.
    
    workstream:

--- dist/ctxmenu.js
+++ dist/ctxmenu.js
@@ -1,4 +1,34 @@
 "use strict";
-window.addEventListener("contextmenu", function (event) {
-    console.log("context menu trigerred", event);
-});
+var ContextMenu = /** @class */ (function () {
+    function ContextMenu() {
+    }
+    ContextMenu.attach = function (target, menu) {
+        var t = typeof target === "string" ? document.querySelector(target) : target;
+        if (t) {
+            window.addEventListener("contextmenu", function (e) {
+                if (e.target !== t) {
+                    return;
+                }
+                var container = document.createElement("ul");
+                menu.forEach(function (item) {
+                    var li = document.createElement("li");
+                    li.innerHTML = item.text;
+                    li.title = item.title || "";
+                    container.appendChild(li);
+                });
+                Object.assign(container.style, {
+                    position: "fixed",
+                    left: e.offsetX + "px",
+                    top: e.offsetY + "px"
+                });
+                document.body.appendChild(container);
+                e.preventDefault();
+                setTimeout(function () { return container.remove(); }, 2000);
+            });
+        }
+        else {
+            console.error("target element " + target + " not found");
+        }
+    };
+    return ContextMenu;
+}());
--- src/ctxmenu.ts
+++ src/ctxmenu.ts
@@ -1,3 +1,49 @@
-window.addEventListener("contextmenu", (event) => {
-    console.log("context menu trigerred", event);
-});
+interface CTXMInteractive {
+    text: string;
+    title?: string;
+}
+
+interface CTXMAction extends CTXMInteractive {
+    action: Function;
+}
+
+interface CTXMAnchor extends CTXMInteractive {
+    href: string;
+    target?: string;
+}
+
+type CTXMItem = CTXMAnchor | CTXMAction;
+
+type CTXMenu = CTXMItem[];
+
+class ContextMenu {
+    public constructor() {/***/ }
+
+    public static attach(target: Element | string, menu: CTXMenu) {
+        const t = typeof target === "string" ? document.querySelector(target) : target;
+        if (t) {
+            window.addEventListener("contextmenu", e => {
+                if (e.target !== t) {
+                    return;
+                }
+                const container = document.createElement("ul");
+                menu.forEach(item => {
+                    const li = document.createElement("li");
+                    li.innerHTML = item.text;
+                    li.title = item.title || "";
+                    container.appendChild(li);
+                });
+                Object.assign<CSSStyleDeclaration, Partial<CSSStyleDeclaration>>(container.style, {
+                    position: "fixed",
+                    left: e.offsetX + "px",
+                    top: e.offsetY + "px"
+                });
+                document.body.appendChild(container);
+                e.preventDefault();
+                setTimeout(() => container.remove(), 2000);
+            });
+        } else {
+            console.error(`target element ${target} not found`);
+        }
+    }
+}
