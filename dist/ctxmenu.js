5698edb7c5eb88cfb65e5616d057552bffb317b2
Nikolaj Kappler <nikolaj.kappler@sap.com>
2019-01-15 11:11:53 +0100

    Basic functionality working
    links and scripts possible,
    default styling included
    
    Todos for 0.1 release: webpack + babel for minify + docu
    
    next steps:
    - dividers
    - headings
    - submenus
    
    workstream:

--- dist/ctxmenu.js
+++ dist/ctxmenu.js
@@ -1,34 +1,70 @@
 "use strict";
+function itemIsAction(item) {
+    return item.hasOwnProperty("action");
+}
+function itemIsAnchor(item) {
+    return item.hasOwnProperty("href");
+}
 var ContextMenu = /** @class */ (function () {
     function ContextMenu() {
+        var _this = this;
+        window.addEventListener("click", function () { return _this.closeMenu(); });
+        window.addEventListener("resize", function () { return _this.closeMenu(); });
+        window.addEventListener("scroll", function () { return _this.closeMenu(); });
     }
-    ContextMenu.attach = function (target, menu) {
+    ContextMenu.prototype.attach = function (target, ctxmenu) {
+        var _this = this;
         var t = typeof target === "string" ? document.querySelector(target) : target;
         if (t) {
             window.addEventListener("contextmenu", function (e) {
-                if (e.target !== t) {
-                    return;
+                if (e.target === t) {
+                    e.stopImmediatePropagation();
+                    //close any open menu
+                    _this.closeMenu();
+                    var container_1 = document.createElement("ul");
+                    ctxmenu.forEach(function (item) {
+                        var li = document.createElement("li");
+                        li.innerHTML = item.text;
+                        li.title = item.title || "";
+                        if (itemIsAction(item)) {
+                            li.addEventListener("click", function () { return item.action(); });
+                            li.className = "interactive";
+                        }
+                        else if (itemIsAnchor(item)) {
+                            li.innerHTML = "<a href=\"" + item.href + "\" target=\"" + (item.target || "") + "\">" + item.text + "</a>";
+                            li.className = "interactive";
+                        }
+                        container_1.appendChild(li);
+                    });
+                    Object.assign(container_1.style, {
+                        position: "fixed",
+                        left: e.offsetX + "px",
+                        top: e.offsetY + "px"
+                    });
+                    container_1.className = "ctxmenu";
+                    _this.menu = container_1;
+                    document.body.appendChild(container_1);
+                    e.preventDefault();
                 }
-                var container = document.createElement("ul");
-                menu.forEach(function (item) {
-                    var li = document.createElement("li");
-                    li.innerHTML = item.text;
-                    li.title = item.title || "";
-                    container.appendChild(li);
-                });
-                Object.assign(container.style, {
-                    position: "fixed",
-                    left: e.offsetX + "px",
-                    top: e.offsetY + "px"
-                });
-                document.body.appendChild(container);
-                e.preventDefault();
-                setTimeout(function () { return container.remove(); }, 2000);
             });
         }
         else {
             console.error("target element " + target + " not found");
         }
     };
+    ContextMenu.prototype.closeMenu = function () {
+        this.menu && this.menu.remove();
+    };
     return ContextMenu;
 }());
+Object.assign(window, {
+    ContextMenu: new ContextMenu()
+});
+document.addEventListener("readystatechange", function (e) {
+    if (document.readyState === "interactive") {
+        //insert default styles as css -> low priority
+        var styles = document.createElement("style");
+        styles.innerHTML = "\n            ul.ctxmenu {\n                border: 1px solid #999;\n                padding: 2px 0;\n                box-shadow: 3px 3px 3px #aaa;\n                background: #fff;\n            }\n            ul.ctxmenu li {\n                margin: 1px 0;\n                padding: 2px 15px;\n                display: block;\n            }\n            ul.ctxmenu li a {\n                color: inherit;\n                text-decoration: none;\n            }\n            ul.ctxmenu li.interactive {\n                cursor: pointer;\n            }\n            ul.ctxmenu li.interactive:hover {\n                background: rgba(0,0,0,0.1);\n            }\n        ";
+        document.head.insertBefore(styles, document.head.childNodes[0]);
+    }
+});
--- src/ctxmenu.ts
+++ src/ctxmenu.ts
@@ -16,34 +16,99 @@ type CTXMItem = CTXMAnchor | CTXMAction;
 
 type CTXMenu = CTXMItem[];
 
-class ContextMenu {
-    public constructor() {/***/ }
+function itemIsAction(item: CTXMItem): item is CTXMAction {
+    return item.hasOwnProperty("action");
+}
 
-    public static attach(target: Element | string, menu: CTXMenu) {
+function itemIsAnchor(item: CTXMItem): item is CTXMAnchor {
+    return item.hasOwnProperty("href");
+}
+
+class ContextMenu {
+    private menu: HTMLUListElement | undefined;
+    public constructor() {
+        window.addEventListener("click", () => this.closeMenu());
+        window.addEventListener("resize", () => this.closeMenu());
+        window.addEventListener("scroll", () => this.closeMenu());
+    }
+
+    public attach(target: Element | string, ctxmenu: CTXMenu) {
         const t = typeof target === "string" ? document.querySelector(target) : target;
         if (t) {
             window.addEventListener("contextmenu", e => {
-                if (e.target !== t) {
-                    return;
+                if (e.target === t) {
+                    e.stopImmediatePropagation();
+                    //close any open menu
+                    this.closeMenu();
+
+                    const container = document.createElement("ul");
+                    ctxmenu.forEach(item => {
+                        const li = document.createElement("li");
+                        li.innerHTML = item.text;
+                        li.title = item.title || "";
+                        if (itemIsAction(item)) {
+                            li.addEventListener("click", () => item.action());
+                            li.className = "interactive";
+                        }
+                        else if (itemIsAnchor(item)) {
+                            li.innerHTML = `<a href="${item.href}" target="${item.target || ""}">${item.text}</a>`;
+                            li.className = "interactive";
+                        }
+                        container.appendChild(li);
+                    });
+                    Object.assign<CSSStyleDeclaration, Partial<CSSStyleDeclaration>>(container.style, {
+                        position: "fixed",
+                        left: e.offsetX + "px",
+                        top: e.offsetY + "px"
+                    });
+                    container.className = "ctxmenu";
+                    this.menu = container;
+                    document.body.appendChild(container);
+
+                    e.preventDefault();
                 }
-                const container = document.createElement("ul");
-                menu.forEach(item => {
-                    const li = document.createElement("li");
-                    li.innerHTML = item.text;
-                    li.title = item.title || "";
-                    container.appendChild(li);
-                });
-                Object.assign<CSSStyleDeclaration, Partial<CSSStyleDeclaration>>(container.style, {
-                    position: "fixed",
-                    left: e.offsetX + "px",
-                    top: e.offsetY + "px"
-                });
-                document.body.appendChild(container);
-                e.preventDefault();
-                setTimeout(() => container.remove(), 2000);
             });
         } else {
             console.error(`target element ${target} not found`);
         }
     }
+
+    public closeMenu() {
+        this.menu && this.menu.remove();
+    }
 }
+
+Object.assign(window, {
+    ContextMenu: new ContextMenu()
+});
+
+document.addEventListener("readystatechange", e => {
+    if (document.readyState === "interactive") {
+        //insert default styles as css -> low priority
+        const styles = document.createElement("style");
+        styles.innerHTML = `
+            ul.ctxmenu {
+                border: 1px solid #999;
+                padding: 2px 0;
+                box-shadow: 3px 3px 3px #aaa;
+                background: #fff;
+            }
+            ul.ctxmenu li {
+                margin: 1px 0;
+                padding: 2px 15px;
+                display: block;
+            }
+            ul.ctxmenu li a {
+                color: inherit;
+                text-decoration: none;
+            }
+            ul.ctxmenu li.interactive {
+                cursor: pointer;
+            }
+            ul.ctxmenu li.interactive:hover {
+                background: rgba(0,0,0,0.1);
+            }
+        `;
+        document.head.insertBefore(styles, document.head.childNodes[0]);
+    }
+});
