c5eaa07e983634d4cfbb6bc8100e758671318f9e
Nikolaj Kappler <nikolaj.kappler@gmail.com>
2022-07-13 21:58:54 +0200

    fix convertStyles script on UNIX systems

--- util/convertStyles.js
+++ util/convertStyles.js
@@ -7,8 +7,9 @@ whilst being in a typescript environment.
 
 const fs = require("fs");
 
-const lines = fs.readFileSync("./src/styles.css", "utf-8").split("\r\n");
+let lines = fs.readFileSync("./src/styles.css", "utf-8").split("\n");
 const styles = lines.map(line => line
+    .replace(/\r/g, "")         // remove carriage return for DOS line endings
     .replace(/\s0\./g, " .")    // remove leading zero from fractions, eg '0.4rem' => '.4rem'
     .replace(/\s+\{/, "{")      // remove empty space between selector and rule brackets, eg '.class {' => '.class{'
     .replace(/\:\s+/, ":")      // remove empty space between rule name and rule value, eg 'padding: 1rem' => 'padding:1rem'
