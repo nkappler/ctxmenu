cb7a153853816a60dede5e4df702304255e37fb6
Nikolaj Kappler <info@nkappler.de>
2024-02-22 22:30:59 +0100

    css whitespace optimization

--- util/convertStyles.js
+++ util/convertStyles.js
@@ -11,8 +11,9 @@ let lines = fs.readFileSync("./src/styles.css", "utf-8").split("\n");
 const styles = lines.map(line => line
     .replace(/\r/g, "")         // remove carriage return for DOS line endings
     .replace(/\s0\./g, " .")    // remove leading zero from fractions, eg '0.4rem' => '.4rem'
-    .replace(/\s+\{/, "{")      // remove empty space between selector and rule brackets, eg '.class {' => '.class{'
-    .replace(/\:\s+/, ":")      // remove empty space between rule name and rule value, eg 'padding: 1rem' => 'padding:1rem'
+    .replace(/,\s/g, ",")        // remove whitespace after comma
+    .replace(/\s+\{/g, "{")      // remove empty space between selector and rule brackets, eg '.class {' => '.class{'
+    .replace(/\:\s+/g, ":")      // remove empty space between rule name and rule value, eg 'padding: 1rem' => 'padding:1rem'
     .replace(/^\s+/g, ""))      // remove empty space caused by indentation
     .join("")                   // join lines
     .replace(/;}/g, "}");       // remove trailing semicolon of last rule in a block, eg 'padding:1rem;}' => 'padding:1rem}'
